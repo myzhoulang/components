8f2851adcc5d144a5b611c51632cc0deb0be7ad7
周浪 <zhou.lang@beicaizs.com>
2021-05-20 11:15:53 +0800

    ci: add workflow

--- .umirc.ts
+++ .umirc.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   mode: 'site',
   // more config: https://d.umijs.org/config
 
+  base: '/components',
+  publicPath: '/components/',
   cssLoader: {
     localsConvention: 'camelCase',
   },
