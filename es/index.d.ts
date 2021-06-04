46c25892ed7df1a01859a535979ba969aed843bc
周浪 <zhou.lang@beicaizs.com>
2021-06-04 18:09:00 +0800

    build: 修改打包方式为babel,添加umd打包 feta: 富文本编辑文件上传添加上传钩子

--- es/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Editor } from './Editor';
-export { default as InputNumberRange } from './InputNumberRange';
-export { default as Uploader } from './Uploader';
-export { default as util } from './utils';
